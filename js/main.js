01dd2ccce6339be6305a3f367b717f2f6243ea2f
tundi <juhasztunde89@gmail.com>
2018-07-05 15:26:25 +0200

    Niki changes

--- js/main.js
+++ js/main.js
@@ -321,6 +321,7 @@
    // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
+    autoplayTimeout: 10000,
     dots: true,
     loop: true,
     items: 1
